1adf8ae1b51a74bd079aace59423d37878877b5f
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2026-02-09 18:21:40 -0300

    feat: start config and add mock server for tests

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/uploads-hotel/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/uploads/**",
+      },
+    ],
+    qualities: [75, 100],
+  },
 };
 
 export default nextConfig;
