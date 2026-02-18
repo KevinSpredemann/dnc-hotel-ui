4f3aa811501c681c49d0a5357aa0f69362bda149
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2026-02-18 19:51:11 -0300

    fix: nextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,33 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      // 🔹 Local
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/uploads/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/uploads-hotel/**",
-      },
-
-      // 🔹 Produção (Railway)
-      {
-        protocol: "https",
-        hostname: "dnchotel-production.up.railway.app",
-        pathname: "/uploads/**",
-      },
-      {
-        protocol: "https",
-        hostname: "dnchotel-production.up.railway.app",
-        pathname: "/uploads-hotel/**",
-      },
-    ],
+    domains: ["localhost", "dnchotel-production.up.railway.app"],
   },
 };
 
