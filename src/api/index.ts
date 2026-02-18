a47c9b7c65add4ee6176af2c0332a09b6d48ef2d
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2026-02-18 18:11:56 -0300

    deploy: host config

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const APP_API = process.env.APP_API_URL;
 
 const instance = axios.create({
-  baseURL: APP_API || "http://localhost:3000",
+  baseURL: APP_API ?? "http://localhost:3000",
   timeout: 10000,
 });
 
